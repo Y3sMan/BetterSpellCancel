818b74924a16b2ba3adfb2a0c338dccc1a808f44
Y3sMan <codmaniac89@gmail.com>
2022-04-03 19:44:28 -0700

    Fixed spell-weapon dual-wielding

--- src/BetterSpellCancel.ts
+++ src/BetterSpellCancel.ts
@@ -1,17 +1,31 @@
+import { printConsole, once, Game } from "@skyrim-platform/skyrim-platform";
 import { hooks } from  "skyrimPlatform";
 
 var r = false
+var r_equipped
+var r_event = ''
 var l = false
+var l_equipped
+var l_event = 'WeapOutLeftReplaceForceEquip'
+export function pl() { return Game.getPlayer(); }
 hooks.sendAnimationEvent.add({
     enter(ctx) {
         let e = ctx.animEventName
         if ( e.toLowerCase().includes('mrh_spell') && e.toLowerCase().includes('start') ) { r = true };
-        if ( (e.toLowerCase().includes('mlh_spell') && e.toLowerCase().includes('start')) || (e.toLowerCase().includes('dualmagic_spell') && e.toLowerCase().includes('start')) ) { l = true };
         if ( e.toLowerCase().includes('mrh_spellrelease_event') ) { r = false };
+
+        if ( (e.toLowerCase().includes('mlh_spell') && e.toLowerCase().includes('start')) || (e.toLowerCase().includes('dualmagic_spell') && e.toLowerCase().includes('start')) ) { l = true };
         if ( e.toLowerCase().includes('mlh_spellrelease_event') ) { l = false };
+        once('update', () => {
+            l_equipped = pl().getEquippedItemType(1)
+            r_equipped = pl().getEquippedItemType(0)
+            if ( l_equipped == 9 && r_equipped == 9 ){ l_event = 'MagicForceEquip'; r_event = 'MagicForceEquip'}
+            else if ( l_equipped != 9 && r_equipped == 9){ l_event = 'WeapOutLeftReplaceForceEquip'}
+            else if ( l_equipped == 9 && r_equipped != 9){ r_event = 'magic_equip'}
+        });
         if ( e.toLowerCase().includes('unequip')) { 
-            if ( r ) { ctx.animEventName = 'MagicForceEquip'; r = false;  };
-            if ( l ) { ctx.animEventName = 'MagicForceEquip'; l = false;  };
+            if ( r ) { ctx.animEventName = r_event; r = false;  };
+            if ( l ) { ctx.animEventName = l_event; l = false;  };
         }
     },
     leave() {}
